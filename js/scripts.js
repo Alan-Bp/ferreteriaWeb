80df73215592df481a0c4a4dd57de2b518d8d60f
Alan-Bp <05aabalp@gmail.com>
2024-10-06 21:27:12 -0600

    ADD-Initial Project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.banner-slider .slide');
+console.log("Página cargada correctamente.");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) slide.classList.add('active');
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
