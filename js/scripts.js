95a2cc5750528ce9873d015b7b23cb764c66e768
Alan-Bp <05aabalp@gmail.com>
2024-10-18 12:43:48 -0600

    ADD-Confguraciones

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,25 @@ let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 
 function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(${(i - index) * 100}%)`;
-    });
+    // Cada imagen ocupa el 50%, así que multiplicamos por 50 en lugar de 100
+    document.querySelector('.slides').style.transform = `translateX(${index * -50}%)`;
 }
 
 function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length; // Ciclo a través de las slides
+    // Asegúrate de que solo haya dos slides para un ciclo continuo
+    currentSlide = (currentSlide + 1) % (slides.length - 1); // Ajusta el ciclo
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     showSlide(currentSlide);
 }
 
 setInterval(nextSlide, 5000); // Cambia de imagen cada 5 segundos
-
 showSlide(currentSlide);
+
+
 
 document.querySelector('.logo img').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const db = new sqlite3.Database('./database/db.sqlite');
 
 // Ruta para obtener productos con paginación
 router.get('/', (req, res) => {
-    const limit = parseInt(req.query.limit) || 60;  // Número de productos por carga
+    const limit = parseInt(req.query.limit) || 70000;  // Número de productos por carga
     const offset = parseInt(req.query.offset) || 0; // Posición inicial
 
     const sql = 'SELECT * FROM productos LIMIT ? OFFSET ?';
